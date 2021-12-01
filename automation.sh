0a72de89d1505ed348d7a7767b4d79b170f8259b
Monjur Hasan <monjurh@vmware.com>
2021-11-30 19:22:43 -0500

    adding task 3

--- automation.sh
+++ automation.sh
@@ -29,7 +29,29 @@ logFileName="$name-httpd-logs-$timestamp.tar"
 cd "/var/log/apache2/"
 tar -zcvf $logFileName *.log 
 
+fileSize=$(du -smh $logFileName | awk '{print $1}')
+
 mv $logFileName /tmp
 
 aws s3 cp /tmp/$logFileName s3://$bucket_name/$logFileName
 
+inventoryFile="/var/www/html/inventory.html"
+cronFile="/etc/cron.d/automation"
+tab="&nbsp;&nbsp;&nbsp;&nbsp;"
+lineBreak="<br><br>"
+header="<b>Log Type</b> $tab $tab $tab <b>Date Created</b> $tab $tab $tab <b>Type</b> $tab $tab $tab <b>Size</b>"
+
+
+if [ ! -f $inventoryFile ]; then
+    touch $inventoryFile
+    echo $header >> $inventoryFile
+fi 
+
+echo $lineBreak >> $inventoryFile
+
+echo "httpd-logs $tab $tab $tab $timestamp $tab $tab tar $tab $tab $tab $fileSize" >> $inventoryFile
+
+if [ ! -f "$cronFile" ]; then
+    touch $cronFile
+    echo "* * * * * root /root/Automation_Project/automation.sh" >> $cronFile
+fi
